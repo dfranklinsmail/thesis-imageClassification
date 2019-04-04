91986f29fe5d718ea1da008fb9cb271a1659f586
Daniel Franklin <dfranklinsmail@gmail.com>
2019-04-03 22:47:40 -0400

    cleaned up comments

--- ssCNN5Layer512.py
+++ ssCNN5Layer512.py
@@ -14,9 +14,9 @@ import torch.optim as optim
 EPOCHS = 2
 BATCH_SIZE = 32
 LEARNING_RATE = 0.003
-TRAIN_DATA_PATH = "./proteins6NewSorted/training"
-VALIDATION_DATA_PATH = "./proteins6NewSorted/validating"
-TEST_DATA_PATH = "./proteins6NewSorted/testing"
+TRAIN_DATA_PATH = "./proteins6NewSorted25PDB/training"
+VALIDATION_DATA_PATH = "./proteins6NewSorted25PDB/validating"
+TEST_DATA_PATH = "./proteins6NewSorted25PDB/testing"
 
 D8244_DATA_PATH = "./proteinsD8244"
 FC699_DATA_PATH = "./proteinsFC699"
@@ -140,9 +140,9 @@ def trainNet(net, batch_size, n_epochs, learning_rate):
     computeAccuracy(net, loss, validation_data_loader, 'validate data')
     computeAccuracy(net, loss, test_data_loader, 'test epoch')
    
-    #computeAccuracy(net, loss, d8244_data_loader, 'D8244 dataset')
-    computeAccuracy(net, loss, d1185_data_loader, 'D1185 dataset')
-    computeAccuracy(net, loss, fc699_data_loader, 'FC699 dataset')
+    computeAccuracy(net, loss, d8244_data_loader, 'D8244 dataset')
+    #computeAccuracy(net, loss, d1185_data_loader, 'D1185 dataset')
+    #computeAccuracy(net, loss, fc699_data_loader, 'FC699 dataset')
 
 # def computeAccuracy(net, loss, accuracy_data_loader, title):
 #   total_val_loss = 0
@@ -289,7 +289,7 @@ def computeAccuracy(net, loss, accuracy_data_loader, title):
   print("incorrect d images predicted b {}".format(total_d_b))
   print("incorrect d images predicted c {}".format(total_d_c))
   
-class CNN(nn.Module):
+class CNN5Layer(nn.Module):
     
       #Our batch shape for input x is (3, 32, 32)
     
@@ -355,58 +355,57 @@ class CNN(nn.Module):
           x = self.fc2(x)
           return(x)
 
-# class CNN(nn.Module):
+class CNN(nn.Module):
     
-#     #Our batch shape for input x is (3, 32, 32)
+    #Our batch shape for input x is (3, 32, 32)
     
-#     def __init__(self):
-#         super(CNN, self).__init__()
+    def __init__(self):
+        super(CNN, self).__init__()
         
-#         #Input channels = 3, output channels = 18
-#         self.conv1 = torch.nn.Conv2d(3, 18, kernel_size=3, stride=1, padding=1)
-#         self.pool1 = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        #Input channels = 3, output channels = 18
+        self.conv1 = torch.nn.Conv2d(3, 18, kernel_size=3, stride=1, padding=1)
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
-#          #Input channels = 16, output channels = 64
-#         self.conv2 = torch.nn.Conv2d(18, 64, kernel_size=3, stride=1, padding=1)
-#         self.pool2 = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+         #Input channels = 18, output channels = 64
+        self.conv2 = torch.nn.Conv2d(18, 64, kernel_size=3, stride=1, padding=1)
+        self.pool2 = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
-#         #4608 input features, 64 output features (see sizing flow below)
-#         #self.fc1 = torch.nn.Linear(18 * 16 * 16, 64)
-#         self.fc1 = torch.nn.Linear(64 * 128 * 128, 64)
+        #1048576 input features, 64 output features (see sizing flow below)
+        self.fc1 = torch.nn.Linear(64 * 128 * 128, 64)
         
-#         #64 input features, 10 output features for our 10 defined classes
-#         self.fc2 = torch.nn.Linear(64, 4)
+        #64 input features, 4 output features for our 4 defined classes
+        self.fc2 = torch.nn.Linear(64, 4)
         
-#     def forward(self, x):
+    def forward(self, x):
         
-#         #Computes the activation of the first convolution
-#         #Size changes from (3, 32, 32) to (18, 32, 32)
-#         x = F.relu(self.conv1(x))
+        #Computes the activation of the first convolution
+        #Size changes from (3, 512, 512) to (18, 32, 32)
+        x = F.relu(self.conv1(x))
         
-#         #Size changes from (18, 32, 32) to (18, 16, 16)
-#         x = self.pool1(x)
+        #Size changes from (18, 32, 32) to (18, 16, 16)
+        x = self.pool1(x)
         
-#         #Computes the activation of the first convolution
-#         #Size changes from (3, 32, 32) to (18, 32, 32)
-#         x = F.relu(self.conv2(x))
+        #Computes the activation of the first convolution
+        #Size changes from (3, 32, 32) to (18, 32, 32)
+        x = F.relu(self.conv2(x))
         
-#         #Size changes from (18, 32, 32) to (18, 16, 16)
-#         x = self.pool2(x)
+        #Size changes from (18, 32, 32) to (18, 16, 16)
+        x = self.pool2(x)
         
-#         #Reshape data to input to the input layer of the neural net
-#         #Size changes from (18, 16, 16) to (1, 4608)
-#         #Recall that the -1 infers this dimension from the other given dimension
-#         #x = x.view(-1, 18 * 16 *16)
-#         x = x.view(-1, 64 * 128 * 128)
+        #Reshape data to input to the input layer of the neural net
+        #Size changes from (18, 16, 16) to (1, 4608)
+        #Recall that the -1 infers this dimension from the other given dimension
+        #x = x.view(-1, 18 * 16 *16)
+        x = x.view(-1, 64 * 128 * 128)
           
-#         #Computes the activation of the first fully connected layer
-#         #Size changes from (1, 4608) to (1, 64)
-#         x = F.relu(self.fc1(x))
+        #Computes the activation of the first fully connected layer
+        #Size changes from (1, 4608) to (1, 64)
+        x = F.relu(self.fc1(x))
         
-#         #Computes the second fully connected layer (activation applied later)
-#         #Size changes from (1, 64) to (1, 10)
-#         x = self.fc2(x)
-#         return(x)
+        #Computes the second fully connected layer (activation applied later)
+        #Size changes from (1, 64) to (1, 10)
+        x = self.fc2(x)
+        return(x)
 
 if __name__ == '__main__':
   CNN = CNN()
